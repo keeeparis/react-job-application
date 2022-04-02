6f357b5c3674d845f388949b48360c7853897b0c
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-02 21:24:58 +0600

    move to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import 'antd/dist/antd.css';
 import './index.css'
 import App from './App'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
-ReactDOM.render(
-    <React.StrictMode>
+const container = document.getElementById('root') as HTMLDivElement
+ReactDOMClient
+    .createRoot(container)
+    .render(
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+    )
