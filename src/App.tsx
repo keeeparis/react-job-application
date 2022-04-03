0a7933e7581186466d9ccbd2cc3a62512a85a1c1
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-03 11:21:50 +0600

    1

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         dispatch(getCatsFetch(page))
         setPage(page => ++page)
     }
-
+    
     useEffect(() => {
         loadMoreHandler()
     }, [])
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import App from './App'
+import { store } from './redux/store'
+
 import 'antd/dist/antd.css';
 import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
 
 const container = document.getElementById('root') as HTMLDivElement
 ReactDOMClient
