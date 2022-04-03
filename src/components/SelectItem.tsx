36371a6e7b6d70c22613d25f2d6df49fa3e6e4fe
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-03 21:35:32 +0600

    handling select change

--- src/components/SelectItem.tsx
+++ src/components/SelectItem.tsx
@@ -1,18 +1,41 @@
 import { Select } from 'antd'
+import { DataType } from '../App'
 import { places } from '../mock'
+import { setCurrentRoute, updateRoute } from '../redux/features/route/routeSlice'
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
 
 const { Option } = Select
 
-const SelectItem = () => {
-    const handleChange = (value: any) => { 
-        console.log(value) 
+type SelectItemProps = {
+    defaultValue: string, 
+    record: DataType['from'] | DataType['to'],
+    id: DataType['id']
+}
+
+const SelectItem = ({ defaultValue, record, id } : SelectItemProps) => {
+    const dispatch = useAppDispatch()
+    const currentRouteId = useAppSelector(state => state.routes.currentRoute.id)
+    
+    const handleChange = (value: any) => {
+        dispatch(updateRoute({
+            value: value.label,
+            record,
+            id
+        }))
+        if (currentRouteId === id) {
+            dispatch(setCurrentRoute(id.toString()))
+        }
     }
+
     return (
         <Select 
-            defaultValue='Point A' 
-            size='middle' 
-            style={{ width: '150px' }} 
+            defaultValue={defaultValue} 
             onChange={handleChange}
+            labelInValue={true}
+            size='middle' 
+            style={{ 
+                width: '150px' 
+            }} 
         >
             {places.map(place =>
                 <Option 
