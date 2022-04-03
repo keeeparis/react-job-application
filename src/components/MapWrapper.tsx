22f65ffc30edb0b24a66206526c4ce09ded304e7
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-03 21:34:53 +0600

    deleted unused marker

--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import RoutingMachine from './RoutingMachine'
 
 const MapWrapper = () => {
     return (
         <>
             <MapContainer 
-                center={[51.505, -0.09]} 
-                zoom={13} 
+                center={[51.555, -0.09]} 
+                zoom={11} 
                 style={{ height: '400px' }}
+                
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                {/* <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker> */}
+                />
                 <RoutingMachine />
             </MapContainer>
         </>
