3851855f59af095616eb40a79e824e22b610aa66
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-03 21:34:41 +0600

    update mock data

--- src/mock/index.tsx
+++ src/mock/index.tsx
@@ -1,29 +1,32 @@
+import { DataType } from "../App"
 import SelectItem from "../components/SelectItem"
 
 export const places = [
-    {value: 'point-A', name: 'Point A'},
-    {value: 'point-B', name: 'Point B'},
-    {value: 'point-C', name: 'Point C'},
+    // with array of number it breaks!
+    // TODO: on change -> manage value properly
+    { value: [51.505, -0.09], name: 'Point A' },
+    { value: [51.555, -0.03], name: 'Point B' },
+    { value: [51.525, -0.05], name: 'Point C' },
 ]
 
-export const dataSource = [
+export const dataSource: DataType[] = [
     {
         key: '1',
-        id: '1',
-        from: <SelectItem />,
-        to: <SelectItem />,
+        id: 1,
+        from: 'Point A',
+        to: 'Point B',
     },
     {
         key: '2',
-        id: '2',
-        from: <SelectItem />,
-        to: <SelectItem />,
+        id: 2,
+        from: 'Point B',
+        to: 'Point C',
     },
     {
         key: '3',
-        id: '3',
-        from: <SelectItem />,
-        to: <SelectItem />,
+        id: 3,
+        from: 'Point C',
+        to: 'Point C',
     },
 ]
 
@@ -37,10 +40,24 @@ export const columns = [
         title: 'Погрузка',
         dataIndex: 'from',
         key: 'from',
+        render: (defaultValue: string, record: DataType) => (
+            <SelectItem 
+                defaultValue={defaultValue} 
+                record={record.from}
+                id={record.id}
+            />
+        )
     },
     {
         title: 'Разгрузка',
         dataIndex: 'to',
         key: 'to',
+        render: (defaultValue: string, record: DataType) => (
+            <SelectItem 
+                defaultValue={defaultValue} 
+                record={record.to}
+                id={record.id} 
+            />
+        )
     },
 ]
