48e6c39c76b9d3da7f255f3c172bbde8837a2fe7
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-03 21:34:30 +0600

    new route slice

---
+++ src/redux/features/route/routeSlice.ts
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { DataType } from "../../../App"
+
+interface routeState {
+    routes: DataType[],
+    currentRoute: DataType,
+    isLoading: boolean,
+    status: 'idle' | 'updated'
+}
+
+const initialState: routeState = {
+    routes: [],
+    currentRoute: {
+        id: 999,
+        key: '999',
+        from: '',
+        to: '',
+    },
+    isLoading: false,
+    status: 'idle'
+}
+
+export const routeSlice = createSlice({
+    name: 'routes',
+    initialState,
+    reducers: {
+        setCurrentRoute: (state, action) => {
+            const route = state.routes.filter(route => route.key === action.payload)[0]
+            state.currentRoute = route
+        },
+        setRoutes: (state, action) => {
+            state.routes = action.payload
+        },
+        updateRoute: (state, action) => {
+            // find route by id
+            const route = state.routes.filter(route => route.id === action.payload.id)[0]
+            // compare previous value (record) with current route 'from' and 'to' values
+            // if they are the same, it means we are changing current select
+            // if not, it must be another select
+            if (route.from === action.payload.record) {
+                route.from = action.payload.value
+            } else {
+                route.to = action.payload.value
+            }
+        }
+    }
+})
+
+export const { setCurrentRoute, updateRoute, setRoutes } = routeSlice.actions
+
+export default routeSlice.reducer
