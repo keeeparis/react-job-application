4d308738c37170505cf0a0152e5de15ce1aaaa40
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-04 14:08:52 +0600

    del saga files

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import createSagaMiddleWare from 'redux-saga'
 
 import routeReducer from '../features/route/routeSlice'
-import routeSaga from '../saga/routeSaga'
-
-const saga = createSagaMiddleWare()
 
 export const store = configureStore({
     reducer: {
         routes: routeReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saga)
+    }
 })
-saga.run(routeSaga)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
