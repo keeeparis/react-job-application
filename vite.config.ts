32a85c2660481ed63d17ab935a94b27b30a91191
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-04 14:07:40 +0600

    deployment setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-job-application/',
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true
       }
     }
-  }
+  },
 })
